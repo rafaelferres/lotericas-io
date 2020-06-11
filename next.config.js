5a9745f15dd7b91bec55807a31ee3d6372e443ad
rafaelferres <raferres@outlook.com.br>
2020-06-11 15:21:42 -0300

    init

---
+++ next.config.js
@@ -0,0 +1,33 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const withSass = require("@zeit/next-sass");
+
+const withTsconfigPaths = (nextConfig ={}) => ({
+    ...nextConfig,
+    ...{
+        webpack(config, options){
+            const newConfig = {
+                ...config,
+                resolve: {
+                    ...config.resolve,
+                    plugins: [
+                        ...(config.resolve && config.resolve.plugins),
+                        new TsconfigPathsPlugin(),
+                    ],
+                },
+            }
+
+            if(typeof nextConfig.webpack === "function"){
+                return nextConfig.webpack(newConfig, options);
+            }
+
+            return newConfig;
+        }
+    }
+});
+
+module.exports = withTsconfigPaths(
+    withSass({
+        cssModules: true,
+    }),
+)
+
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import 'bootstrap/scss/bootstrap.scss';
+
+const HomePage = () => (
+    <div>
+        Oi!
+    </div>
+)
+
+export default HomePage;
