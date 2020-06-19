25e1fe18453650fea2934d10007266ebd52ca018
rafaelferres <raferres@outlook.com.br>
2020-06-19 15:00:22 -0300

    Change github icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,7 @@ export default class HeaderStatic extends Component {
                         </div>
                         <div>
                             <a href="https://github.com/rafaelferres/lotericas-io" target="_new" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">
-                                <FontAwesomeIcon icon={faGithub} />
+                                GitHub
                             </a>
                         </div>
                     </div>
